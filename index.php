b4e26d000ea6b58dd4beeeace95d2a894be7fd09
linghu258 <ranger0504@hotmail.com>
2019-11-16 15:42:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ and open the template in the editor.
         <meta name="keywords" content="restaurant, catering, menu, reservation, home, food, open, 7">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/header_footer.css">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css" type="text/css" />
 
@@ -22,9 +24,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            include "navbar.inc.php";
+        include "navbar.inc.php";
         ?>
-        
+
     <main>
         <section class="container-fluid">
             <section id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -36,19 +38,19 @@ and open the template in the editor.
                 <!-- Wrapper for slides -->
                 <section class="carousel-inner">
                     <section class="item active">
-                        <img src="image/restaurant3wide.jpg" alt="reservationimage">
+                        <img src="image/restaurant3wide.jpg" class="img-responsive" alt="reservationimage">
                         <section class="carousel-caption">
                             <p>WELCOME</p>
-                            <button onclick="window.location.href='reservation.php'" class="btncaro">RESERVE A
+                            <button onclick="window.location.href = 'reservation.php'" class="btncaro">RESERVE A
                                 TABLE</button>
                         </section>
                     </section>
 
                     <section class="item">
-                        <img src="image/catering.jpg" alt="cateringimage">
+                        <img src="image/catering.jpg" class="img-responsive" alt="cateringimage">
                         <section class="carousel-caption">
                             <p>CATERING</p>
-                            <button onclick="window.location.href='catering.php'" class="btncaro">ORDER NOW</button>
+                            <button onclick="window.location.href = 'catering.php'" class="btncaro">ORDER NOW</button>
                         </section>
                     </section>
                 </section>
@@ -77,45 +79,46 @@ and open the template in the editor.
                 </section>
 
                 <figure id="home" class="col-sm-6">
-                     <img src="image/kitchen.jpg" class="img-responsive" alt="kitchen">
+                    <img src="image/kitchen.jpg" class="img-responsive" alt="kitchen">
                     <!--Url: https://unsplash.com/photos/zk5vJ8Duw9k | Author: Hansel Louis | Publish: August, 2019 | Publisher: Unsplash | Visited: October 5, 2019-->
                 </figure>
             </section>
-            <button onclick="window.location.href='about_contact.html'" class="btn-RM">READ MORE</button>
+            <button onclick="window.location.href = 'about_contact.html'" class="btn-RM">READ MORE</button>
         </section>
     </main>
 
     <!-- Admin Login Modal Form -->
     <div id="adminLogin" class="modal">
-        <span onclick="document.getElementById('adminLogin').style.display='none'" class="close"
+        <span onclick="document.getElementById('adminLogin').style.display = 'none'" class="close"
               title="Close Modal">&times;
         </span>
 
         <!-- Modal Content -->
-        <form class="modal-content animate" action="admin/process_login.php" method="post">
+        <form class="modal-content animate" action="admin/admin_dashboard.php">
 
             <div class="flex-container">
-                <label for="email"><b>Enter Email</b></label>
-                <input type="email" id="email" placeholder="Enter email" name="email" pattern="[^@]+@[^@]+\.[a-zA-Z]{2,}" required>
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="username" required>
 
-                <label for="password"><b>Enter Password</b></label>
-                <input type="password" id="password" placeholder="Enter Password" name="password" required>
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
 
                 <button type="submit" id="loginbtn">Login</button>
                 <label>
-                    <input type="checkbox" id="rememberme" checked="checked"> Remember me
+                    <input type="checkbox" checked="checked" name="remember"> Remember me
                 </label>
             </div>
 
             <div class="containerbottom">
-                <button type="button" onclick="document.getElementById('adminLogin').style.display='center'"
-                        class="cancelbtn btn btn-dark" data-dismiss="modal">Cancel</button>
+                <button type="button" onclick="document.getElementById('adminLogin').style.display = 'none'"
+                        class="cancelbtn" data-dismiss="modal">Cancel</button>
+                <span class="password"><a href="#">Forgot password?</a></span>
             </div>
         </form>
     </div>
-        
-        <?php
-            include "footer.inc.php";
-        ?>
-    </body>
+
+    <?php
+    include "footer.inc.php";
+    ?>
+</body>
 </html>
