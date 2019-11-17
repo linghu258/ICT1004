20f7e00f7382e008113df6e91c5cc63e636bb89b
hasyna-iman <57612738+hasyna-iman@users.noreply.github.com>
2019-11-17 15:45:24 +0800

    Add files via upload

--- admin/adminFooter.inc.php
+++ admin/adminFooter.inc.php
@@ -1,5 +1,11 @@
+<!DOCTYPE html>
+<html  lang="en">
+    <head>
+        <title>TUMMY FOR YUMMY</title>
+    </head>
 <footer id="adminFooter">
         <section class="container">
             <p>Copyright YUM YUM © 2019. All rights reserved.</p>
         </section>
 </footer>
+</html>
