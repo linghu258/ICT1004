eb1bb3bdb3735c179c0d59175573e110b6cd8299
linghu258 <ranger0504@hotmail.com>
2019-11-18 23:39:50 +0800

    Update reservation.php

--- reservation.php
+++ reservation.php
@@ -4,9 +4,10 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html lang="en">
+<html>
     <head>
         <title>TUMMY FOR YUM-YUM</title>
+        <link rel="icon" href="image/1004 Logo 2.png">
         <meta name="description" content="YUM-YUM's restaurant and catering services reservation form. Book your table online here! Easy and hassle-free!">
         <meta name="keywords" content="restaurant, menu, reservation, book, online, easy">
         <meta charset="UTF-8">
@@ -35,26 +36,26 @@ and open the template in the editor.
             <section class="form-group">
                 <label>First Name:</label>
                 <input type="text" class="form-control" placeholder="Enter first name" id="res_First_Name" name="res_First_Name" pattern="/^([a-zA-Z']+)$/"
-                       required>
+                       title="Please input a name" required="true">
 
                 <label>Last Name:</label>
                 <input type="text" class="form-control" placeholder="Enter last name" id="res_Last_Name" name="res_Last_Name" pattern="/^([a-zA-Z']+)$/"
-                       required>
+                       title="Please input a name" required="true">
 
                 <label>Email address:</label>
                 <input type="email" class="form-control" placeholder="Enter email" id="resEmail" name="resEmail"
                        pattern="/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/"
-                       required>
+                       title="Please input a valid email" required="true">
 
                 <label>Contact Number:</label>
                 <input type="tel" class="form-control" placeholder="Enter contact number" id="res_ContactNumber" name="res_ContactNumber" pattern="/^([0-9]{8})$/"
-                       maxlength="8" required>
+                       maxlength="8" title="please input a valid number" required="true">
 
                 <label>Date:</label>
-                <input type="date" class="form-control" id="resDate" name="resDate" required>
+                <input type="date" class="form-control" id="resDate" name="resDate" required="true">
 
                 <label>Time:</label> <br>
-                <select id="resTime" name="resTime" required>
+                <select id="resTime" name="resTime" required="true">
                     <option value="0">--Select Time--</option>
                     <option value="5pm">5:00 PM</option>
                     <option value="6pm">6:00 PM</option>
@@ -66,11 +67,11 @@ and open the template in the editor.
                 <br>
 
                 <label>Number of people:</label>
-                <input type="tel" class="form-control" placeholder="Enter pax" id="resPax" name="resPax" pattern="/^([0-9])$/" maxlength="3"
-                       required>
+                <input type="tel" class="form-control" placeholder="Enter pax" id="resPax" name="resPax" pattern="/^([0-9])+$/" maxlength="3"
+                       title="Please input a vaid number" required="true">
 
                 <label>Request:</label><br>
-                <textarea id="resComment" name="resComment" placeholder="Request (optional)"></textarea>
+                <textarea id="resComment" id="res_comment" name="resComment" placeholder="Request (optional)"></textarea>
             </section>
 
             <button type="submit" id="btnReservation" class="btn btn-default">Submit Reservation</button>
