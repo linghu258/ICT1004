3e6811d9808cd7d3bcd6136286a8f44401a8e0d9
linghu258 <ranger0504@hotmail.com>
2019-11-19 10:15:29 +0800

    Update process_reservation.php

--- process_reservation.php
+++ process_reservation.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/header_footer.css">
+        <link rel="stylesheet" href="css/process_reservation.css">
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
