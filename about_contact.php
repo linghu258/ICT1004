e3554a6cd89594dded211f03011a07867772d7f9
hasyna-iman <57612738+hasyna-iman@users.noreply.github.com>
2019-11-19 21:23:34 +0800

    Update about_contact.php

--- about_contact.php
+++ about_contact.php
@@ -4,7 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="en">
     <head>
         <title>TUMMY FOR YUM-YUM</title>
         <link rel="icon" href="image/1004 Logo 2.png">
