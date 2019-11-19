ed3dbb8852717edc7b26d15ae4feb33e419f4164
hasyna-iman <57612738+hasyna-iman@users.noreply.github.com>
2019-11-19 21:06:58 +0800

    Update footer.inc.php

--- footer.inc.php
+++ footer.inc.php
@@ -7,27 +7,27 @@
 ?>
 
 <footer>
-    <section class="container">
-        <section class="row">
+    <div class="container">
+        <div class="row">
             <section class="col-sm-4">
-                <h1><u>LOCATE US</u></h1>
+                <h2><u>LOCATE US</u></h2>
                 <p>172 Ang Mo Kio Avenue 8, 567739</p>
                 <a data-target="#adminLogin" data-toggle="modal" href="#adminLogin" class="footer-link-txt-responsiveness">ADMINSTRATION</a>
             </section>
             <section class="col-sm-4">
-                <h1><u>OPENING HOURS</u></h1>
+                <h2><u>OPENING HOURS</u></h2>
                 <p>Monday - Sunday</p>
                 <p>Time: 12pm - 11pm</p>
                 <p>Not open on public holiday</p>
             </section>
             <section class="col-sm-4">
-                <h1><u>CONTACT</u></h1>
+                <h2><u>CONTACT</u></h2>
                 <p><a href="mailto:info@domain.com" title="glorythemes" class="footer-link-txt-responsiveness">ICT1004@sit.singaporetech.edu..com</a></p>
                 <p>+65 1800 222 3333</p>
                 <p>172 Ang Mo Kio Avenue 8, 567739</p>
             </section>
 
-            <section>
+            
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-12 text-center">
@@ -35,7 +35,7 @@
                         </div>
                     </div>
                 </div>
-            </section>
-        </section>
-    </section>
+            
+        </div>
+    </div>
 </footer>
